a658c2bd7939d661b06da021f62dfee7071a9cdd
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-08-02 18:35:19 -0700

    taking break from Portfolio

---
+++ portfolio/src/components/index.js
@@ -0,0 +1,18 @@
+import Header from './Header'
+import SocketConnection from './SocketConnection';
+
+function App() {
+    return (
+        <div className="App container">
+            <div className="row">
+                <div className="card col-12">
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ portfolio/src/data.js
@@ -0,0 +1,53 @@
+// import all of your images from your assets/images/current project//
+
+// export default [
+//     {
+//         "id": 1,
+//         "name": "Trivia With A Twist",
+//         "image": "TWAT",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     },
+//     {
+//         "id": 2,
+//         "name": "",
+//         "image": "",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     },
+//     {
+//         "id": 3,
+//         "name": "",
+//         "image": "",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     },
+//     {
+//         "id": 4,
+//         "name": "",
+//         "image": "",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     },
+//     {
+//         "id": 5,
+//         "name": "",
+//         "image": "",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     },
+//     {
+//         "id": 6,
+//         "name": "",
+//         "image": "",
+//         "github": "githublink",
+//         "deploy": "deployed link"
+
+//     }
+
+// ]
